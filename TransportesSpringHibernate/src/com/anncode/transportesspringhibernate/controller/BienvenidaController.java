6174b71a1375e67f812f2295c853e829b2407c0e
Ann <anahi.anndroid@gmail.com>
2016-07-16 11:32:21 -0500

    Hola mundo Spring

---
+++ TransportesSpringHibernate/src/com/anncode/transportesspringhibernate/controller/BienvenidaController.java
@@ -0,0 +1,16 @@
+package com.anncode.transportesspringhibernate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BienvenidaController {
+	
+	@RequestMapping("/bienvenida")
+	public ModelAndView mostrarBienvenida() {
+		String mensaje = "<h1>Bienvenidos jajaja</h1>";
+		return new ModelAndView("bienvenida", "mensaje", mensaje);
+	}
+
+}
